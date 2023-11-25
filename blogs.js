939fb599e2341ad3026a03d822f366ef267a922b
Forkedinsane <143539219+Forkedinsane@users.noreply.github.com>
2023-11-25 20:20:13 +0530

    feat:Updated vlog

--- blogs.js
+++
@@ -1,8 +0,0 @@
-function store(){
-    let title=document.getElementById("h3");
-    let text=document.getElementById("blogtxt");
-    localStorage.setItem("title",title.value);
-    localStorage.setItem("text",text.value);
-    document.getElementById("sec1").children[0].classList.add("blogs");
-    // document.getElementById("sec1").children[0].innerHTML.localStorage.getItem;
-}
